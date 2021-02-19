87598309b2d5b888359407029f04f67307d397b3
eneeesyk <54284965+eneeesyk@users.noreply.github.com>
2021-02-19 18:44:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,14 @@ snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
 
+game_is_on = True
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
@@ -30,18 +31,17 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.score += 1
-        scoreboard.refresh_score()
+        scoreboard.increase_score()
 
 #   #collision with wall
     if snake.head.xcor() > 285 or snake.head.xcor() < -285 or snake.head.ycor() > 285 or snake.head.ycor() < -285:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
 #   #collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 screen.exitonclick()
